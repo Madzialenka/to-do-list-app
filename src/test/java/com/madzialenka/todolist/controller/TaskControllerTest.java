bde372edf185123776e18ada8552bdca56031b73
Magdalena Ceglińska <m.ceglinska1999@gmail.com>
2020-12-07 19:31:32 +0100

    * remaining methods from TaskController tested

--- src/test/java/com/madzialenka/todolist/controller/TaskControllerTest.java
+++ src/test/java/com/madzialenka/todolist/controller/TaskControllerTest.java
@@ -1,11 +1,18 @@
 package com.madzialenka.todolist.controller;
 
+import com.madzialenka.todolist.dto.TaskRequestDTO;
+import com.madzialenka.todolist.dto.TaskResponseDTO;
 import com.madzialenka.todolist.service.TaskService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 class TaskControllerTest {
@@ -21,7 +28,47 @@ class TaskControllerTest {
 
     @Test
     void deleteTask() {
-        underTest.deleteTask(1L);
-        Mockito.verify(taskService).deleteTask(1L);
+        long id = 1L;
+        underTest.deleteTask(id);
+        Mockito.verify(taskService).deleteTask(id);
+    }
+
+    @Test
+    void getTasks() {
+        Sort.Direction direction = Sort.Direction.ASC;
+        String sortBy = "field";
+        List<TaskResponseDTO> expected = Arrays.asList(
+                Mockito.mock(TaskResponseDTO.class),
+                Mockito.mock(TaskResponseDTO.class)
+        );
+        Mockito.when(taskService.getTasks(sortBy, direction)).thenReturn(expected);
+        List<TaskResponseDTO> actual = underTest.getTasks(sortBy, direction);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void createTask() {
+        TaskRequestDTO taskRequestDTO = Mockito.mock(TaskRequestDTO.class);
+        TaskResponseDTO expected = Mockito.mock(TaskResponseDTO.class);
+        Mockito.when(taskService.createTask(taskRequestDTO)).thenReturn(expected);
+        TaskResponseDTO actual = underTest.createTask(taskRequestDTO);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void updateTask() {
+        TaskRequestDTO taskRequestDTO = Mockito.mock(TaskRequestDTO.class);
+        Long id = 1L;
+        TaskResponseDTO expected = Mockito.mock(TaskResponseDTO.class);
+        Mockito.when(taskService.updateTask(id, taskRequestDTO)).thenReturn(expected);
+        TaskResponseDTO actual = underTest.updateTask(id, taskRequestDTO);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void deleteTasks() {
+        List<Long> ids = Arrays.asList(1L, 2L);
+        underTest.deleteTasks(ids);
+        Mockito.verify(taskService).deleteTasks(ids);
     }
 }
